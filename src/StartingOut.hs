6f56a100e293b92863d13d425e22a77bdc1341fb
Daisuke <Yamagutino@greenApple.local>
2017-04-22 18:39:01 +0900

    fixed points and mlen

--- src/StartingOut.hs
+++ src/StartingOut.hs
@@ -5,10 +5,10 @@ module StartingOut (manlen, points) where
 -- The Manhattan distance 'manlen(p1, p2)' where p1 = (a, b) and p2 = (c, d) is defined by the following equation.
 -- manlen(p1, p2) = abs(a - c) + abs(b - d)
 manlen :: (Int, Int) -> (Int, Int) -> Int
-manlen p q = abs (fst p - fst q) + abs (snd p - snd q)
+manlen p q = undefined
 
 -- List of Lattice points
 -- Define functions points that accept nonnegative integers and return a list of integer pairs whose absolute values are within that number.
 -- Forexample, When Integer 1 is given, points returns [(x,y) | x in [-1,0,-1], y in [-1, 0, 1]]
 points :: Int -> [(Int, Int)]
-points n = [(p,q) | p <- [-n..n], q <- [-n..n]]
+points n = undefined
